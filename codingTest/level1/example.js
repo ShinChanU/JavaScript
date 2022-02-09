8118923f7523a614fda30e398701da660c22f512
ShinChanU <tlscksdn963@naver.com>
2022-02-09 16:49:40 +0900

    level/모의고사 done

--- codingTest/level1/example.js
+++ codingTest/level1/example.js
@@ -6,21 +6,49 @@ const solution = (answers) => {
   let a = [1, 2, 3, 4, 5];
   let b = [2, 1, 2, 3, 2, 4, 2, 5];
   let c = [3, 3, 1, 1, 2, 2, 4, 4, 5, 5];
+  const result = [];
+
+  let aLen = a.length;
+  let bLen = b.length;
+  let cLen = c.length;
 
   let aCount = 0;
   let bCount = 0;
   let cCount = 0;
 
-  for(let i = 0; i < answers.length; i++) {
-    let temp = answers[i];
-    aCount = a.indexOf(temp) === i ? aCount+1 : aCount;
-    bCount = b.indexOf(temp) === i ? bCount+1 : bCount;
-    cCount = c.indexOf(temp) === i ? cCount+1 : cCount;
-  }
+  answers.map((v, i) => {
+    let aL = i%aLen;
+    let bL = i%bLen;
+    let cL = i%cLen;
+    a.splice(i, 1, a[aL]);
+    b.splice(i, 1, b[bL]);
+    c.splice(i, 1, c[cL]);
+
+    if(v === a[i]) aCount++;
+    if(v === b[i]) bCount++;
+    if(v === c[i]) cCount++;
+  });
 
   console.log(aCount, bCount, cCount);
 
-  return ;
+  const maxNum = Math.max(aCount, bCount, cCount);
+  if(maxNum === 0) {
+    return [1, 2, 3];
+  }
+  if(maxNum === aCount) {
+    result.push(1);
+  }
+  if(maxNum === bCount) {
+    result.push(2);
+  } 
+  if(maxNum === cCount) {
+    result.push(3);
+  } 
+
+  return result;
 };
 
-console.log(solution([1, 3, 2, 4, 2])); 
+console.log(solution([0, 0, 0, 0, 0, 0, 2])); 
+
+// 0209 done
+// 완전 탐색..
