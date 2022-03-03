3de000da02a2a3833d76a9087c97ab4487a87093
ShinChanU <tlscksdn963@naver.com>
2022-03-03 16:00:40 +0900

    level2/targetNumber(DFS)

---
+++ codingTest/level2/targetNumber.js
@@ -0,0 +1,50 @@
+// numbers의 요소를 적당히 더하거나 빼서 target으로 만드는데 성공하는 경우의 수
+
+// 재귀함수...
+// 단순 for문.. or map...
+// x는 그래프 인덱스, hap은 합 경우의 수, result는 결과에 맞는 경우의 수 개수
+const recursiveFn = (numbers, target, x, hap, result, numbersLen) => {
+  // 전위 순회
+  if (x > Math.pow(2, numbersLen + 1) - 1) return result;
+  else {
+    if (x % 2 === 0) hap += numbers[Math.floor(Math.log2(x) - 1)];
+    if (x % 2 === 1 && x != 1) hap -= numbers[Math.floor(Math.log2(x) - 1)];
+    if (x > Math.pow(2, numbersLen) && target === hap) result++;
+  }
+
+  result = recursiveFn(numbers, target, x * 2, hap, result, numbersLen);
+  result = recursiveFn(numbers, target, x * 2 + 1, hap, result, numbersLen);
+
+  return result;
+};
+
+// const solution = (numbers, target) => {
+//   let hap = 0,
+//     result = 0;
+//   const numbersLen = numbers.length;
+//   return recursiveFn(numbers, target, 1, hap, result, numbersLen);
+// };
+
+// console.log(solution([1, 1, 1, 1, 1], 3));
+console.log(solution([4, 1, 2, 1], 4));
+// console.log(solution([1, 2, 3, 5], 5));
+
+function solution(numbers, target) {
+  let answer = 0;
+  getAnswer(0, 0);
+  function getAnswer(x, value) {
+    console.log(x, value, numbers[x]);
+    if (x < numbers.length) {
+      getAnswer(x + 1, value + numbers[x]);
+      getAnswer(x + 1, value - numbers[x]);
+    } else {
+      if (value === target) {
+        answer++;
+      }
+    }
+  }
+  return answer;
+}
+
+// 220303 doing
+// 다시 풀고 velog 올리기
