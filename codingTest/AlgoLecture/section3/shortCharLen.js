b2135485d60a9ba79690bf26570aca7bd0ff3b58
ShinChanU <tlscksdn963@naver.com>
2022-03-17 22:29:44 +0900

    sec3 (4, 5) 강의 보고 수정

--- codingTest/AlgoLecture/section3/shortCharLen.js
+++ codingTest/AlgoLecture/section3/shortCharLen.js
@@ -18,4 +18,36 @@ const solution = (s, t) => {
 
 console.log(solution("teachermode", "e"));
 
-// 0314
+// 0314, O(n^2) 방식
+
+const solution2 = (s, t) => {
+  const answer = [];
+  let p = 1000; // 충분히 큰 수
+
+  for (let x of s) {
+    if (x === t) {
+      p = 0;
+      answer.push(p);
+    } else {
+      p++;
+      answer.push(p);
+    }
+  }
+
+  p = 1000;
+  for (let i = s.length - 1; i >= 0; i--) {
+    if (s[i] === t) p = 0;
+    else {
+      p++;
+      answer[i] = Math.min(answer[i], p);
+    }
+  }
+
+  return answer;
+};
+
+console.log(solution2("teachermode", "e"));
+
+// 0317
+// O(n) 방식(강의)
+// 오른쪽, 왼쪽으로 루프 2번만 하면 가능
--- codingTest/AlgoLecture/section3/stringCompress.js
+++ codingTest/AlgoLecture/section3/stringCompress.js
@@ -16,3 +16,23 @@ const solution = (str) => {
 console.log(solution("KKHSSSSSSSE")); // K2HS7E
 
 // 0315
+
+const solution2 = (str) => {
+  let answer = "";
+  let count = 1;
+  str += " ";
+  for (let i = 0; i < str.length - 1; i++) {
+    if (str[i] === str[i + 1]) count++;
+    else {
+      answer += str[i];
+      if (count > 1) answer += String(count);
+      count = 1;
+    }
+  }
+  return answer;
+};
+
+console.log(solution2("KKHSSSSSSSE")); // K2HS7E
+
+// 0317
+// 강의 문제 풀이(내용은 비슷)
