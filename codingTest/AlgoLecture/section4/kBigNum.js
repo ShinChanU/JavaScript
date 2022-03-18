e61cb7c4024e1cf28713e6cfa743c39f809f3c42
ShinChanU <tlscksdn963@naver.com>
2022-03-19 00:18:21 +0900

    sec4 5 문제 풂

--- codingTest/AlgoLecture/section4/kBigNum.js
+++ codingTest/AlgoLecture/section4/kBigNum.js
@@ -1,3 +1,20 @@
-const solution = () => {};
+const solution = (k, arr) => {
+  let result = [];
 
-console.log(solution());
+  for (let i = 0; i < arr.length; i++) {
+    for (let j = 0; j < arr.length; j++) {
+      for (let k = 0; k < arr.length; k++) {
+        if (j === i || j === k || i === k) continue;
+        else result.push(arr[i] + arr[j] + arr[k]);
+      }
+    }
+  }
+
+  const answer = [...new Set(result)].sort((a, b) => a - b).reverse();
+
+  return answer[k - 1];
+};
+
+console.log(solution(3, [13, 15, 34, 23, 45, 65, 33, 11, 26, 42]));
+
+// 0318 done
