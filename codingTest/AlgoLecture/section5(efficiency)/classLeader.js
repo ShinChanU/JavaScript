9352eedcb6911a6c2f6659d9664884f635bbcff8
ShinChanU <tlscksdn963@naver.com>
2022-03-28 14:41:52 +0900

    sec5, 6 수정

--- codingTest/AlgoLecture/section5(efficiency)/classLeader.js
+++ codingTest/AlgoLecture/section5(efficiency)/classLeader.js
@@ -43,3 +43,32 @@ const solution = (str) => {
 console.log(solution("BACBACCACCBDEDE"));
 
 // 0328 done.
+
+// javascript Map 자료구조 사용 (get, set, has, value, keys... )
+const solution2 = (str) => {
+  let max = 0;
+  let sH = new Map();
+
+  for (let i = 0; i < str.length; i++) {
+    if (sH.has(str[i])) sH.set(str[i], sH.get(str[i]) + 1);
+    else sH.set(str[i], 1);
+  }
+
+  // 내가 푼 풀이 (짧지만 가독성)
+  // max = Math.max(...sH.values());
+  // return [...sH.keys()][[...sH.values()].indexOf(max)];
+
+  // 강의에서 푼 풀이 (for)
+  let answer;
+  for (let [key, val] of sH) {
+    if (val > max) {
+      max = val;
+      answer = key;
+    }
+  }
+  return answer;
+};
+
+console.log(solution2("BACBACCACCBDEDE"));
+
+// 0328 done.
