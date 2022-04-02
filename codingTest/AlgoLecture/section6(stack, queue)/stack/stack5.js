2df40a04735f359b1cef616fe021a386b7f0b362
ShinChanU <tlscksdn963@naver.com>
2022-04-03 00:24:07 +0900

    sec6 , stack5 done

--- codingTest/AlgoLecture/section6(stack, queue)/stack/stack5.js
+++ codingTest/AlgoLecture/section6(stack, queue)/stack/stack5.js
@@ -1,26 +1,21 @@
-const solution = (str) => {
+const solution = (s) => {
   let answer = 0;
-  let stack1 = [];
-  let stack2 = [];
+  let stack = [];
   let i = 0;
 
-  while (i < str.length) {
-    let x = str[i];
-    let y = str[i + 1];
+  while (i < s.length) {
+    let x = s[i];
+    let y = s[i + 1];
     if (x === "(") {
       if (y === ")") {
-        if (stack1.length !== 0) stack2.push(x, y);
+        if (stack.length !== 0) answer += stack.length;
         i++;
       } else {
-        stack1.push(x);
+        stack.push(x);
       }
     } else {
-      stack1.pop();
-      answer += stack2.length / 2 + 1; // (()(())) 예시 해결 X
-      console.log(i, stack1, stack2);
-      if (stack1.length === 0) {
-        stack2 = [];
-      }
+      stack.pop();
+      answer += 1;
     }
     i++;
   }
@@ -29,6 +24,28 @@ const solution = (str) => {
 };
 
 console.log(solution("()(((()())(())()))(())"));
-// console.log(solution("(((()(()()))(())()))(()())"));
+console.log(solution("(((()(()()))(())()))(()())"));
 
 // 0403 doing 강의 듣기
+
+const solution2 = (s) => {
+  let answer = 0;
+  let stack = [];
+
+  for (let i = 0; i < s.length; i++) {
+    if (s[i] === "(") stack.push(s[i]);
+    else {
+      stack.pop();
+      if (s[i - 1] === "(") {
+        answer += stack.length;
+      } else answer += 1;
+    }
+  }
+
+  return answer;
+};
+
+console.log(solution2("()(((()())(())()))(())"));
+console.log(solution2("(((()(()()))(())()))(()())"));
+
+// 0403 강의 보고 수정
