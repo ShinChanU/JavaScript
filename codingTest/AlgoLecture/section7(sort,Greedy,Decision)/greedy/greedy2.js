80b5062ada917c596f721e4988f4a0f2fc315d8d
ShinChanU <tlscksdn963@naver.com>
2022-04-16 19:50:34 +0900

    sec 7, greedy

--- codingTest/AlgoLecture/section7(sort,Greedy,Decision)/greedy/greedy2.js
+++ codingTest/AlgoLecture/section7(sort,Greedy,Decision)/greedy/greedy2.js
@@ -2,6 +2,7 @@ const solution = (arr) => {
   let answer = [];
 
   arr.sort((a, b) => a[0] - b[0]);
+  console.log(arr);
 
   for (let i = 0; i < arr.length - 1; i++) {
     let tmp = arr[i];
@@ -36,4 +37,43 @@ console.log(
   ])
 ); // output 2
 
-// 0413 study
+// 0416 lecture 끝나는 시간을 기준으로 greedy
+
+const solution2 = (arr) => {
+  let answer = 0;
+
+  arr.sort((a, b) => {
+    // 끝나는 시간이 같을 경우에는 시작시간으로 정렬
+    if (a[1] === b[1]) return a[0] - b[0];
+    else return a[1] - b[1];
+  });
+  console.log(arr);
+
+  let et = 0;
+  for (let x of arr) {
+    if (x[0] >= et) {
+      answer++;
+      et = x[1];
+    }
+  }
+
+  return answer;
+};
+
+console.log(
+  solution2([
+    [1, 4],
+    [2, 3],
+    [3, 5],
+    [4, 6],
+    [5, 7],
+  ])
+);
+
+console.log(
+  solution2([
+    [3, 3],
+    [1, 3],
+    [2, 3],
+  ])
+);
--- codingTest/AlgoLecture/section7(sort,Greedy,Decision)/greedy/greedy3.js
+++ codingTest/AlgoLecture/section7(sort,Greedy,Decision)/greedy/greedy3.js
@@ -27,3 +27,41 @@ console.log(
 );
 
 // 0413 study
+
+const solution2 = (arr) => {
+  let answer = 0;
+  let cnt = 0;
+
+  let tmp = [];
+  for (let x of arr) {
+    tmp.push([x[1], "e"]);
+    tmp.push([x[0], "s"]);
+  }
+
+  tmp.sort((a, b) => {
+    if (a[0] === b[0]) return a[1].charCodeAt() - b[1].charCodeAt();
+    else return a[0] - b[0];
+  });
+
+  for (let i = 0; i < tmp.length; i++) {
+    if (tmp[i][1] === "e") cnt--;
+    if (tmp[i][1] === "s") {
+      cnt++;
+      if (answer < cnt) answer = cnt;
+    }
+  }
+
+  return answer;
+};
+
+console.log(
+  solution2([
+    [14, 18],
+    [12, 15],
+    [15, 20],
+    [20, 30],
+    [5, 14],
+  ])
+);
+
+// 0416 lecture 들어야함
