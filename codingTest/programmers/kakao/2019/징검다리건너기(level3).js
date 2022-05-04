17d77ef717bf63d31f8f0aa278d3ea32c258b02d
ShinChanU <tlscksdn963@naver.com>
2022-05-04 16:44:45 +0900

    0504 study

--- codingTest/programmers/kakao/2019/징검다리건너기(level3).js
+++ codingTest/programmers/kakao/2019/징검다리건너기(level3).js
@@ -4,33 +4,59 @@ const solution = (stones, k) => {
   let n = stones.length;
 
   while (1) {
-    if (cnt >= k) break;
     for (let i = 0; i < n; i++) {
-      console.log(stones, i, result);
-
-      if (stones[i] !== 0) stones[i]--;
-      else {
-        console.log("여기");
+      if (stones[i] !== 0) {
+        stones[i]--;
+        cnt = 0;
+      } else {
+        cnt++;
         let x = 1;
         while (1) {
           if (stones[i + x] !== 0 || i + x >= n) {
-            // console.log(i, stones, "i추가");
+            cnt = 0;
             break;
           } else {
-            // i+x 값이 0 일때
-            console.log(i, x, cnt, stones);
             cnt++;
             x++;
             if (cnt === k) break;
           }
         }
-        // console.log(i, stones);
+        if (cnt === k) break;
       }
     }
+    if (cnt === k) break;
     result++;
   }
 
   return result;
 };
 
-console.log(solution([2, 4, 5, 3, 2, 1, 4, 2, 5, 1], 3)); // 3
+// console.log(solution([2, 4, 5, 3, 2, 1, 4, 2, 5, 1], 3)); // 3
+// 0504 59% pass
+
+// 이번엔 이분 탐색 해보자
+const solution2 = (stones, k) => {
+  let lt = 0;
+  let rt = 200000000;
+
+  while (lt <= rt) {
+    let mid = parseInt((lt + rt) / 2);
+    let cnt = 0;
+    for (let el of stones) {
+      if (el - mid <= 0) {
+        cnt++;
+      } else {
+        cnt = 0;
+      }
+      if (cnt === k) break;
+    }
+
+    if (cnt === k) rt = mid - 1;
+    else lt = mid + 1;
+  }
+
+  return lt;
+};
+
+console.log(solution2([2, 4, 5, 3, 2, 1, 4, 2, 5, 1], 3)); // 3
+// 0504 구글링,,의 힘.. 왜 이건 pass가 될까..
