02396f8c74b6a0c0f5cb6eed77285b800371f12a
Clay <76439533+ShinChanU@users.noreply.github.com>
2022-06-06 16:56:11 +0900

    [Bronze V] Title: 1998년생인 내가 태국에서는 2541년생?!, Time: 144 ms, Memory: 9592 KB -BaekjoonHub

---
+++ 백준/Bronze/18108. 1998년생인 내가 태국에서는 2541년생？！/1998년생인 내가 태국에서는 2541년생？！.js
@@ -0,0 +1,14 @@
+const fs = require("fs");
+const filePath = process.platform === "linux" ? "/dev/stdin" : "./testCase.txt";
+let input = fs
+  .readFileSync(filePath)
+  .toString()
+  .trim()
+  .split("\n")
+  .map((e) => e.trim());
+
+const solution = (num) => {
+  return num - 543;
+};
+
+console.log(solution(Number(input[0])));
